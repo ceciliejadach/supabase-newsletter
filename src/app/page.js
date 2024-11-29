618abdc92171d9d2a5ca8beb0788fd2467f862b2
Cecilie <cepe0001@stud.kea.dk>
2024-11-29 11:15:14 +0100

    m

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { getSubscribers } from "@/lib/supabase";
 import Form from "@/components/Form";
 import Link from "next/link";
-// import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 //når man skriver async, så ved man, at man er på serveren
 export default async function Home() {
@@ -10,18 +10,18 @@ export default async function Home() {
   return (
     <div className="max-w-screen-xl mx-auto">
       <Form />
-      <ul className="flex flex-wrap gap-4 mt-10">
+      {/* <ul className="flex flex-wrap gap-4 mt-10">
         {subscribers.map((subscribe) => (
           <li className="p-4 bg-white shadow-md rounded flex data-[updated='true']:outline-green-600" key={subscribe.id}>
             <Link href={`/update/${subscribe.id}`} className="flex items-center gap-5">
               <p>
                 {subscribe.name} - {subscribe.email}
               </p>
-              {/* <IoIosArrowForward /> */}
+              <IoIosArrowForward />
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
